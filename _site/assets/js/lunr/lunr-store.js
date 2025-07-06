1f5ef7964e74bdbcedd5fc5cead5f3138d022931
jeongtaek06 <jeongteak06@naver.com>
2025-07-06 20:55:06 +0900

    구글, 네이버 검색엔진 등록

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "baekjoon2606",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/posts/baekjoon2606/",
+        "teaser": null
+      }]
